a655857af0a0788de8f5dc2f2cdfbd1f7b0d7f35
Rayan Macapa-ar <macaancasan1@gmail.com>
2024-03-06 13:40:35 +0800

    setup-database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer("unique_id")->unique();
+            $table->string("lastname");
             $table->string('name');
+            $table->
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
