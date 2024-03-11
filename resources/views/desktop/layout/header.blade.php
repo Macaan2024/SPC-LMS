2571f8d657dc46f574b695113e9842ce8778025e
CASAN MACAAN <macaancasan1@gmail.com>
2024-03-12 05:07:33 +0800

    navbar & login modality done

---
+++ resources/views/desktop/layout/header.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/header.css">
+</head>
+<body>
+    
+    <nav class="nav-wrapper container-fluid">
+        <div class="col-12 d-lg-flex align-items-center">
+            <div class="brand col-2">
+                <span>SPC LMS</span>
+            </div>
+            <div class="search col-5 d-flex justify-content-center align-items-center">
+                <label for="" class="">Search</label>
+                <input type="text" class="rounded-pill" name="search">
+            </div>
+            <div class="col-5 d-flex justify-content-between align-items-center">
+                <div class="gmail d-flex align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48"><rect width="48" height="48" fill="none"/><path fill="#ffc107" d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8c-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C12.955 4 4 12.955 4 24s8.955 20 20 20s20-8.955 20-20c0-1.341-.138-2.65-.389-3.917"/><path fill="#ff3d00" d="m6.306 14.691l6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C16.318 4 9.656 8.337 6.306 14.691"/><path fill="#4caf50" d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238A11.91 11.91 0 0 1 24 36c-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44"/><path fill="#1976d2" d="M43.611 20.083H42V20H24v8h11.303a12.04 12.04 0 0 1-4.087 5.571l.003-.002l6.19 5.238C36.971 39.205 44 34 44 24c0-1.341-.138-2.65-.389-3.917"/></svg>
+                    <span>macaancasan1@gmail.com</span>
+                </div>
+                <div class="fb d-flex align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 128 128"><rect width="118.35" height="118.35" x="4.83" y="4.83" fill="#3d5a98" rx="6.53" ry="6.53"/><path fill="#fff" d="M86.48 123.17V77.34h15.38l2.3-17.86H86.48v-11.4c0-5.17 1.44-8.7 8.85-8.7h9.46v-16A126.56 126.56 0 0 0 91 22.7c-13.62 0-23 8.3-23 23.61v13.17H52.62v17.86H68v45.83z"/></svg>
+                    <span>CASAN MACAAN</span>
+                </div>
+                <div class="cp d-flex align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 72 72"><rect width="34.043" height="55.942" x="14.182" y="7.979" fill="#9b9b9a" rx="3.68" ry="3.68"/><path fill="#3f3f3f" d="M44.393 54.996h-26.38a.975.975 0 0 1-.972-.973V15.117a.975.975 0 0 1 .973-.973h26.379a.975.975 0 0 1 .973.973v38.906a.919.919 0 0 1-.973.973"/><path fill="#fcea2b" d="M46.285 7.675a13.032 13.032 0 0 0-9.263 22.203l-2.673 8.581l7.173-5.624a13.032 13.032 0 1 0 4.763-25.16"/><path fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M45.366 37.468v16.555a.919.919 0 0 1-.973.973h-26.38a.975.975 0 0 1-.972-.973V15.117a.975.975 0 0 1 .973-.973h13.558"/><circle cx="31.203" cy="59.252" r="1.945"/><path fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M33.334 11.226H26.34m21.885 25.635v23.38a3.69 3.69 0 0 1-3.68 3.68H17.862a3.69 3.69 0 0 1-3.68-3.68V11.659a3.69 3.69 0 0 1 3.68-3.68H36.97"/><path fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M46.892 26.339v-5.745h-2.903m5.663 5.745h-5.729"/><circle cx="46.589" cy="15.602" r="1.909"/><path fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M46.285 7.675A13.026 13.026 0 0 0 37.256 30.1l-2.604 8.36l7.077-5.548a13.03 13.03 0 1 0 4.556-25.236Z"/></svg>
+                    <span>+639606294089</span>
+                </div>
+                <button type="button" class="button1" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Login</button>
+            </div>
+        </div>  
+    </nav>
+    <!-- ------------------------------------ LOGIN MODALITY HERE ------------------------------------------- -->
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered ">
+            <div class="modal-content">
+                <div class="headmodal modal-header border-0">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body ">
+                    <div class="col-12 d-flex justify-content-center">
+                        <div class="contents col-9">
+                            <div class="login-icon  my-3">
+                                <div class="icon-circle">
+                                    <svg class="" xmlns="http://www.w3.org/2000/svg" width="6em" height="5.5em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4"/></svg>
+                                </div>
+                                
+                            </div>
+                            <h4 class="login-title my-4">LOGIN</h4>
+                            <form>
+                                <div class="user-field mb-3">
+                                    <label for="recipient-name">Student ID / Unique Code:</label>
+                                        <input class="my-1" type="text" name="stud_id"><br>
+                                    <label for="" class="mt-3">Password</label><br>
+                                        <input class="my-1" type="password" name="pass"><br>
+                                </div>
+                                <div class="login-as mt-4 mb-5">
+                                    <span>Login as : </span>
+                                    <select class="p-1">
+                                        <option value="Select Usertype">Select Usertype</option>
+                                        <option value="Faculty">Faculty</option>
+                                        <option value="Student">Student</option>
+                                        <option value="Librarian Staff">Librarian Staff</option>
+                                    </select>
+                                </div>
+                                <div class="btn-submit">
+                                    <input class="mb-1" type="submit" name="submit"><br>
+                                    <a href=""><span>Forgot Password?</span></a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
