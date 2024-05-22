345c7060148087ab05ec5d86b3c0947a114f1d4a
casan macaan <macaancasan1@gmail.com>
2024-05-22 18:18:30 +0800

    5/22/2024 6:17/

--- resources/views/Users/student/pages/Dashboard/requestBook.blade.php
+++ resources/views/Users/student/pages/Dashboard/requestBook.blade.php
@@ -1,29 +1,55 @@
 <x-student.layout>
     <x-slot name="request_book">
-       <div class="container p-0 bg-primary mt-3" style="min-width:420px;">
+       <div class="container p-0 mt-3" style="min-width:420px;">
             <div class="row p-0 m-0">
-                <div class="col-12 d-flex justify-content-center justify-content-sm-start col-sm-6 p-0 m-0 bg-dark">
+                <div class="col-12 d-flex justify-content-center justify-content-sm-start p-0 m-0 px-2">
                     @if ($requestBook->level == 'College')
                         <img src="/books_images/{{ $requestBook->level }}/{{ $requestBook->category }}/{{ $requestBook->image }}" alt="Library Books" style="width:220px; height:250px;">
                     @else
                         <img src="/books_images/{{ $requestBook->level }}/{{ $requestBook->image }}" alt="Library Books">
                     @endif
                 </div>
-                <div class="col-12 col-md-6 p-0 m-0 bg-danger p-1">
-                    <div class="d-flex align-items-start bg-primary" style="height:100%;">
-                        <div class="d-flex flex-column bg-warning px-3 py-0 justify-content-between align-items-start" style="height:100%;">
-                            <h6 class="p-0 m-0 fs-6">ISBN</h6>
-                            <h6 class="p-0 m-0 fs-6">Title</h6>
-                            <h6 class="p-0 m-0 fs-6">Author</h6>
-                            <h6 class="p-0 m-0 fs-6">Publication Year</h6>
-                            <h6 class="p-0 m-0 fs-6">Publication Address</h6>
+                <div class="col-12 d-flex justify-content-between p-0 m-0 mt-2 px-2">
+                    <h6 class="fs-6  text-nowrap">ISBN : {{$requestBook->isbn}}</h6>
+                    <h6 class="fs-6  text-nowrap">Borrowed Records : {{ $requestBook->total_borrow}}</h6>
+                    
+                </div>
+                <hr>
+            </div>
+            <div class="row p-0 m-0">
+                <div class="col-12 col-lg-6 mt-0 mt-md-0 col-md p-0 m-0 p-1">
+                    <div class="d-flex align-items-start" style="height:100%;">
+                        <div class="d-flex flex-column  px-1 py-0 justify-content-between align-items-start" style="height:250px;">
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">ISBN</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">Title</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">Author</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">Publication Year</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">Publication Address</h6>
                         </div>
-                        <div class="d-flex flex-column bg-warning px-3 py-0 justify-content-between align-items-start" style="height:100%;">
-                            <h6 class="p-0 m-0 fs-6">: {{$requestBook->isbn}}</h6>
-                            <h6 class="p-0 m-0 fs-6">: {{$requestBook->title}}</h6>
-                            <h6 class="p-0 m-0 fs-6">: {{$requestBook->author}}</h6>
-                            <h6 class="p-0 m-0 fs-6">: {{$requestBook->publication_year}}</h6>
-                            <h6 class="p-0 m-0 fs-6">: {{$requestBook->publication_address}}</h6>
+                        <div class="d-flex flex-column px-3 py-0 justify-content-between align-items-start" style="height:250px;">
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">: {{$requestBook->isbn}}</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">: {{$requestBook->title}}</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">: {{$requestBook->author}}</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">: {{$requestBook->publication_year}}</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">: {{$requestBook->publication_address}}</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 mt-3 nt-lg-0 mt-lg-0 col-md p-0 m-0 p-1">
+                    <div class="d-flex align-items-start" style="height:100%;">
+                        <div class="d-flex flex-column  px-1 py-0 justify-content-between align-items-start" style="height:250px;">
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">Level</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">Edition</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">Category</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">Pages</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">Quantity</h6>
+                        </div>
+                        <div class="d-flex flex-column ms-5 px-3 py-0 justify-content-between align-items-start" style="height:250px;">
+                            <h6 class="p-0 m-0 fs-6 text-nowrap ms-5">: {{$requestBook->isbn}}</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">: {{$requestBook->title}}</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">: {{$requestBook->author}}</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">: {{$requestBook->publication_year}}</h6>
+                            <h6 class="p-0 m-0 fs-6 text-nowrap">: {{$requestBook->publication_address}}</h6>
                         </div>
                     </div>
                 </div>
--- resources/views/Users/student/pages/Dashboard/viewCategory.blade.php
+++ resources/views/Users/student/pages/Dashboard/viewCategory.blade.php
@@ -32,7 +32,11 @@
                             @if ($book->status == 'Available')
                                 <div class="col  mt-3 d-flex flex-column">
                                     <div class="position-relative p-0 m-0 bg-warning" style="height:100%;">
-                                            <img src="/images/{{ $book->image }}" alt="Library Books" style="height:90%;width:100%;">
+                                        @if ($book->level == 'College')
+                                            <img src="/books_images/{{ $book->level }}/{{ $book->category}}/{{$book->image}}" alt="Library Books" style="height:90%;width:100%;">
+                                        @else 
+                                            <img src="/books_images/{{ $book->level }}/{{$book->image}}" alt="Library Books" style="height:90%;width:100%;">
+                                        @endif
                                             <a href="{{ route('spc-lms.request-book', ['id' => $book->id]) }}" class="bg-success w-100 text-decoration-none text-white fs-6 fw-normal d-flex justify-content-center align-items-center" style="height:10%;">Borrow</a>
                                             <div class="position-absolute top-0 end-0" style="border-left: 70px solid transparent; border-top: 70px solid #661011;"></div>
                                             <span class="position-absolute text-white text-center fs-6" style="top:4%; right:1%; transform: rotate(45deg);">NEW</span>
@@ -41,7 +45,11 @@
                             @else
                                 <div class="col  mt-3 d-flex flex-column">
                                     <div class="position-relative p-0 m-0 bg-warning" style="height:100%;">
-                                            <img src="/images/{{ $book->image }}" alt="Library Books" style="height:90%;width:100%;">
+                                        @if ($book->level == 'College')
+                                            <img src="/books_images/{{ $book->level }}/{{ $book->category}}/{{$book->image}}" alt="Library Books" style="height:90%;width:100%;">
+                                        @else 
+                                            <img src="/books_images/{{ $book->level }}/{{$book->image}}" alt="Library Books" style="height:90%;width:100%;">
+                                        @endif
                                             <a class="w-100 text-decoration-none text-white fs-6 fw-normal d-flex justify-content-center align-items-center" style="background-color:gray;height:10%;">Unavailable</a>
                                             <div class="position-absolute top-0 end-0" style="border-left: 70px solid transparent; border-top: 70px solid #661011;"></div>
                                             <span class="position-absolute text-white text-center fs-6" style="top:4%; right:1%; transform: rotate(45deg);">NEW</span>
@@ -52,7 +60,11 @@
                             @if ($book->status == 'Available')
                                 <div class="col  mt-3 d-flex flex-column">
                                     <div class="position-relative p-0 m-0 bg-warning" style="height:100%;">
-                                            <img src="/images/{{ $book->image }}" alt="Library Books" style="height:90%;width:100%;">
+                                        @if ($book->level == 'College')
+                                            <img src="/books_images/{{ $book->level }}/{{ $book->category}}/{{$book->image}}" alt="Library Books" style="height:90%;width:100%;">
+                                        @else 
+                                            <img src="/books_images/{{ $book->level }}/{{$book->image}}" alt="Library Books" style="height:90%;width:100%;">
+                                        @endif
                                             <a href="{{ route('spc-lms.request-book', ['id' => $book->id]) }}" class="bg-success w-100 text-decoration-none text-white fs-6 fw-normal d-flex justify-content-center align-items-center" style="height:10%;">Borrow</a>
                                             <div class="position-absolute top-0 end-0" style="border-left: 70px solid transparent; border-top: 70px solid gray;"></div>
                                             <span class="position-absolute text-white text-center fs-6" style="top:4%; right:1%; transform: rotate(45deg);">{{$book->publication_year}}</span>
@@ -61,7 +73,11 @@
                             @else
                                 <div class="col  mt-3 d-flex flex-column">
                                     <div class="position-relative p-0 m-0 bg-warning" style="height:100%;">
-                                            <img src="/images/{{ $book->image }}" alt="Library Books" style="height:90%;width:100%;">
+                                        @if ($book->level == 'College')
+                                            <img src="/books_images/{{ $book->level }}/{{ $book->category}}/{{$book->image}}" alt="Library Books" style="height:90%;width:100%;">
+                                        @else 
+                                            <img src="/books_images/{{ $book->level }}/{{$book->image}}" alt="Library Books" style="height:90%;width:100%;">
+                                        @endif
                                             <a class="w-100 text-decoration-none text-white fs-6 fw-normal d-flex justify-content-center align-items-center" style="background-color:gray;height:10%;">Unavailable</a>
                                             <div class="position-absolute top-0 end-0" style="border-left: 70px solid transparent; border-top: 70px solid gray;"></div>
                                             <span class="position-absolute text-white text-center fs-6" style="top:4%; right:1%; transform: rotate(45deg);">{{$book->publication_year}}</span>
